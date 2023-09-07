63e6f698332b7e13adafb87571f16d6a20652be6
gilles <heitaa.gilles1@gmail.com>
2023-09-07 12:08:48 -1000

    stats

--- src/app/app/home/home.page.ts
+++ src/app/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Courses } from 'src/app/models/Courses';
+import { Utilisateurs } from 'src/app/models/Utilisateurs';
+import { CoursesService } from 'src/app/services/courses.service';
+import { UtilisateursService } from 'src/app/services/utilisateurs.service';
 import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
@@ -8,12 +12,20 @@ import { UtilityService } from 'src/app/services/utility.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private utility : UtilityService) { 
+  utilisateurs : Array<Utilisateurs> = [];
+  courses : Array<Courses> = [];
+  utilisateurByDepense : Array<any> = [];
+
+  constructor(private utility : UtilityService,
+              private utilisateursService : UtilisateursService,
+              private coursesService : CoursesService) { 
     this.redirection();
   }
 
-  ngOnInit() {
-    this.redirection();
+  async ngOnInit() {
+    await this.refresh();
+    await this.redirection();
+    await this.statsUtilisateursByDepense();
   }
 
   private async redirection(){
@@ -22,5 +34,42 @@ export class HomePage implements OnInit {
       this.utility.navigateTo('authentification');
     }
   }
+
+  private async refresh(){
+    this.courses = await this.getCourses();
+    this.utilisateurs = await this.getUtilisateurs();
+  }
+
+  private async getUtilisateurs(){
+    return await this.utilisateursService.get();
+  
+  }
+  private async getCourses(){
+    return await this.coursesService.getCourse();
+  }
+
+  public statsUtilisateursByDepense(){
+
+    var result : Array<any> = [];
+    var depenses = 0
+    this.utilisateurs.map(utiliateur => {
+      this.courses.map(course => {
+        if(course.payeurId === utiliateur.id){
+            depenses += course.montantReel
+        }
+      })
+      result.push({
+        utilisateur : utiliateur.libelle,
+        depense : depenses
+      })
+
+      depenses = 0
+    })
+
+    this.utilisateurByDepense = result;
+
+    
+  }
+  
 
 }
