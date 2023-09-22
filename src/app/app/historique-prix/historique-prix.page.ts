fad20a3d816daf5c22361dc6462bad026c4cc692
gilles <heitaa.gilles1@gmail.com>
2023-09-21 18:32:00 -1000

    ajustement historique prix

--- src/app/app/historique-prix/historique-prix.page.ts
+++ src/app/app/historique-prix/historique-prix.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HistoriquePrix } from 'src/app/models/HistoriquePrix';
+import { Magasins } from 'src/app/models/Magasins';
 import { HistoriquePrixService } from 'src/app/services/historique-prix.service';
+import { MagasinsService } from 'src/app/services/magasins.service';
 
 @Component({
   selector: 'app-historique-prix',
@@ -10,13 +12,34 @@ import { HistoriquePrixService } from 'src/app/services/historique-prix.service'
 export class HistoriquePrixPage implements OnInit {
 
   historiqueprix : Array<HistoriquePrix> = [];
+  magasins : Array<Magasins> = [];
   
-  constructor(private historiqueprixservice : HistoriquePrixService) { }
+  constructor(private historiqueprixservice : HistoriquePrixService,
+              private magasinservice : MagasinsService) { }
 
   async ngOnInit() {
     
-    this.historiqueprix = await this.historiqueprixservice.get();
+    await this.refresh();
   
+  }
+
+  public async refresh(){
+    this.historiqueprix = await this.historiqueprixservice.get();
+    this.magasins = await this.getMagasin();
+  }
+
+  public async getMagasin(){
+    const magasins : Array<Magasins> = await this.magasinservice.get();
+    return magasins;
+  }
+
+  public getLibelleMagasin(magasinId : number){
+    return this.magasins.find(magasin => magasin.id == magasinId)?.libelle;
+  }
+
+  public async deleteDefinitivement(historique : HistoriquePrix){
+    await this.historiqueprixservice.deleteDefinitivement(historique);
+    await this.refresh();
   }
 
 
